488579d9fc4c3cf1f56d935351b0456d2f667e70
ymcfy <ymcfyjava@163.com>
2022-03-14 15:57:45 +0800

    master修改提交第二次

--- src/com/ym/day20220314/IndexDemo.java
+++ src/com/ym/day20220314/IndexDemo.java
@@ -8,7 +8,7 @@ package com.ym.day20220314;
 public class IndexDemo {
     public static void main(String[] args) {
         System.out.println("修改");
-        System.out.println("修改1");
+        System.out.println("master修改1");
         System.out.println("修改2");
         System.out.println("修改3");
         System.out.println("master修改");
